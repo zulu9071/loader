f7a81d97f3bbb8db50671e7448bf7f970ed58f6a
zulu9071 <167585045+zulu9071@users.noreply.github.com>
2024-04-20 00:13:25 +0200

    Add files via upload

---
+++ launcher.bat
@@ -0,0 +1,48 @@
+@echo off
+start /min Successfully.vbs
+
+:loop
+cls
+color 4
+echo    /\        ________    ________                                    ________     ______    ____________
+echo   /  \      /            ^|                 ^|   ^|\   ^|         ^|     ^|            ^|               ^|   
+echo  /____\    /________     ^| _______         ^|   ^| \  ^|         ^|     ^|________    ^|               ^| 
+echo /      \           /     ^|       ^|         ^|   ^|  \ ^|         ^|     ^|            ^|               ^|  
+echo/        \ ________/      ^|_______^|         ^|   ^|   \^|    ^|____^|     ^|________    ^|______         ^|
+timeout /t 1 >nul
+
+cls
+color 6
+echo    /\        ________    ________                                    ________     ______    ____________
+echo   /  \      /            ^|                 ^|   ^|\   ^|         ^|     ^|            ^|               ^|   
+echo  /____\    /________     ^| _______         ^|   ^| \  ^|         ^|     ^|________    ^|               ^| 
+echo /      \           /     ^|       ^|         ^|   ^|  \ ^|         ^|     ^|            ^|               ^|  
+echo/        \ ________/      ^|_______^|         ^|   ^|   \^|    ^|____^|     ^|________    ^|______         ^|
+echo                                                VALORANT TRIGGERBOT AND AUTOAIM
+timeout /t 1 >nul
+
+cls
+color 2
+echo    /\        ________    ________                                    ________     ______    ____________
+echo   /  \      /            ^|                 ^|   ^|\   ^|         ^|     ^|            ^|               ^|   
+echo  /____\    /________     ^| _______         ^|   ^| \  ^|         ^|     ^|________    ^|               ^| 
+echo /      \           /     ^|       ^|         ^|   ^|  \ ^|         ^|     ^|            ^|               ^|  
+echo/        \ ________/      ^|_______^|         ^|   ^|   \^|    ^|____^|     ^|________    ^|______         ^|
+echo                                                VALORANT TRIGGERBOT AND AUTOAIM
+echo                                                https://discord.gg/UUceBWFsgg
+timeout /t 1 >nul
+
+cls
+color 3
+echo    /\        ________    ________                                    ________     ______    ____________
+echo   /  \      /            ^|                 ^|   ^|\   ^|         ^|     ^|            ^|               ^|   
+echo  /____\    /________     ^| _______         ^|   ^| \  ^|         ^|     ^|________    ^|               ^| 
+echo /      \           /     ^|       ^|         ^|   ^|  \ ^|         ^|     ^|            ^|               ^|  
+echo/        \ ________/      ^|_______^|         ^|   ^|   \^|    ^|____^|     ^|________    ^|______         ^|
+echo                                                VALORANT TRIGGERBOT AND AUTOAIM
+echo                                                https://discord.gg/UUceBWFsgg
+echo                                                     STATUS: UNDETECTED
+timeout /t 1 >nul
+
+goto loop
+
